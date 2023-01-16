36cb68367d5ac7e6a19c7fc4ffe9eca63d23b175
seyton35 <prosto99-00@mail.ru>
2023-01-16 23:05:20 +0300

    firebase embedding

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { Server } = require('socket.io')
 
 const io = new Server(server)
 
+const admin = require('firebase-admin');
+const serviceAccount = require('./config/firebase_key/serviceAccountKey.json')
 
 const config = require('config')
 const mongoose = require('mongoose')
@@ -13,10 +15,32 @@ const auth = require('./routes/auth.routes')
 const transaction = require('./routes/transaction.routes')
 const dataBase = require('./routes/dataBase.routes')
 
+const SocketReducer = require('./socket/SocketReducer.js')
 
 const PORT = config.get('port') || 5000
 
 app.use(express.json())
+app.use((req, res, next) => {
+    req.io = io
+    return next()
+})
+
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+app.use((req, res, next) => {
+    const firestore = admin.firestore()
+    req.firestore = {
+        Transaction: firestore.collection("transactions"),
+        CurrencyAccounts: firestore.collection("currencyAccounts"),
+        Users: firestore.collection("users")
+    }
+    return next()
+})
+
+
 
 // app.use(express.urlencoded({ extended: false }))
 
@@ -36,64 +60,41 @@ app.post('/', (req, res) => {
 
 async function start() {
     try {
-        io.on('connection', (socket) => {
-            console.log('user ', socket.id, ' connected');
+        // io.on('connection', (socket) => {
+        //     console.log('user ', socket.id, ' connected');
 
-            socket.on('TEST', (data, cb) => {
-                console.log(data);
-                cb(data.split("").reverse().join(""))
-            })
-            socket.on('TEST2', () => {
-                console.log('emit to id');
-                socket.to('6384edf6445b37df2373a9fa').emit('/',
-                    {
-                        way: 'CHECK_YOUR_ISSUES',
-                        id: '6384edf6445b37df2373a9fa'
-                    })
-            })
+        //     socket.on('TEST', (data, cb) => {
+        //         console.log(data);
+        //         cb(data.split("").reverse().join(""))
+        //     })
+        //     socket.on('TEST2', () => {
+        //         console.log('emit to id');
+        //         socket.to('6384edf6445b37df2373a9fa').emit('/',
+        //             {
+        //                 way: 'CHECK_YOUR_ISSUES',
+        //                 id: '6384edf6445b37df2373a9fa'
+        //             })
+        //     })
 
-            socket.on('/', (data, cb) => {
-                SocketReducer(socket, data, cb)
-            })
+        //     socket.on('/', (data, cb) => {
+        //         SocketReducer(socket, data, cb)
+        //     })
 
-            // transaction(socket)
-            return socket.on('MONEY_REQUEST:get', (data, cb) => {
-                console.log(data)
-                cb
-            })
+        //     // transaction(socket)
+        //     return socket.on('MONEY_REQUEST:get', (data, cb) => {
+        //         console.log(data)
+        //         cb
+        //     })
 
-        })
+        // })
 
         await mongoose.connect(config.get('DB_URL'))
 
         server.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
     } catch (e) {
-        console.log("Server error", e.message);
+        console.log("Server error:", e.message);
         process.exit()
     }
 }
 
-const axios = require('axios');
-const { SocketReducer } = require('./socket/SocketReducer')
-const flag = true
-
-if (flag) {
-    start()
-} else axi()
-
-
-
-async function axi() {
-    try {
-        const res = await axios.post(
-            'http://192.168.31.254:3000/',
-            { num: 5 },
-        )
-        if (res.status == 200) console.log('status 200');
-        else console.log('wrong status')
-        console.log(res.data)
-    } catch (e) {
-        console.error('e.response.data.message');
-        console.error(e.response.data.message);
-    }
-}
+start()
