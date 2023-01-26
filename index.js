3ea377765fc4ad228c2b13c2b3baab6885d6b8de
seyton35 <prosto99-00@mail.ru>
2023-01-26 21:30:48 +0300

    escaping vercel and soket.io

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-// const { Server } = require('socket.io')
 
-// const io = new Server(server)
 
 const admin = require('firebase-admin')
 const serviceAccount = require('./config/firebase_key/serviceAccountKey.json')
@@ -14,17 +12,9 @@ const auth = require('./routes/auth.routes')
 const transaction = require('./routes/transaction.routes')
 const dataBase = require('./routes/dataBase.routes')
 
-const SocketReducer = require('./socket/SocketReducer.js')
-
 const PORT = config.get('port') || 5000
 
 app.use(express.json())
-// app.use((req, res, next) => {
-//     req.io = io
-//     return next()
-// })
-
-
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -40,9 +30,6 @@ app.use((req, res, next) => {
     return next()
 })
 
-
-
-// app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/auth', auth)
 app.use('/api/transaction', transaction)
@@ -60,34 +47,6 @@ app.get('/', (req, res) => {
 
 async function start() {
     try {
-        // io.on('connection', (socket) => {
-        //     console.log('user ', socket.id, ' connected');
-
-        //     socket.on('TEST', (data, cb) => {
-        //         console.log(data);
-        //         cb(data.split("").reverse().join(""))
-        //     })
-        //     socket.on('TEST2', () => {
-        //         console.log('emit to id');
-        //         socket.to('6384edf6445b37df2373a9fa').emit('/',
-        //             {
-        //                 way: 'CHECK_YOUR_ISSUES',
-        //                 id: '6384edf6445b37df2373a9fa'
-        //             })
-        //     })
-
-        //     socket.on('/', (data, cb) => {
-        //         SocketReducer(socket, data, cb)
-        //     })
-
-        //     // transaction(socket)
-        //     return socket.on('MONEY_REQUEST:get', (data, cb) => {
-        //         console.log(data)
-        //         cb
-        //     })
-
-        // })
-
         server.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
     } catch (e) {
         console.log("Server error:", e.message);
--- socket/transaction.js
+++
@@ -1,6 +0,0 @@
-export function transaction (socket) {
-    return socket.on('MONEY_REQUEST/get', (data, cb) => {
-        console.log(data)
-        cb
-    })
-}
